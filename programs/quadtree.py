cbc4f71e282c8fa1f7827ed2a541b9ac83c6dd7d
jenniferouyang <70252312+jenniferouyang@users.noreply.github.com>
2023-11-16 23:04:04 -0500

    change

--- programs/quadtree.py
+++ programs/quadtree.py
@@ -12,9 +12,9 @@ from Graph import Graph
 
 
 class QuadTreeNode:
-    def __init__(self, x, y, id):
-        self.x = x
-        self.y = y
+    def __init__(self, lat, lon, id):
+        self.lat = lat
+        self.lon = lon
         self.id = id
 
 class QuadTree:
@@ -25,36 +25,42 @@ class QuadTree:
         self.divided = False
 
     def subdivide(self):
-        x, y, w, h = self.boundary
-        nw = QuadTree((x, y, w/2, h/2), self.capacity)
-        ne = QuadTree((x + w/2, y, w/2, h/2), self.capacity)
-        sw = QuadTree((x, y + h/2, w/2, h/2), self.capacity)
-        se = QuadTree((x + w/2, y + h/2, w/2, h/2), self.capacity)
-        self.nw = nw
-        self.ne = ne
-        self.sw = sw
-        self.se = se
+        min_lat, min_lon, max_lat, max_lon = self.boundary
+        mid_lat = (min_lat + max_lat) / 2
+        mid_lon = (min_lon + max_lon) / 2
+
+        self.northwest = QuadTree((min_lat, min_lon, mid_lat, mid_lon), self.capacity)
+        self.northeast = QuadTree((min_lat, mid_lon, mid_lat, max_lon), self.capacity)
+        self.southwest = QuadTree((mid_lat, min_lon, max_lat, mid_lon), self.capacity)
+        self.southeast = QuadTree((mid_lat, mid_lon, max_lat, max_lon), self.capacity)
+
         self.divided = True
 
-    def insert(self, point):
-        if not self.in_boundary(point):
+    def insert(self, vertex):
+        if not self.in_boundary(vertex.lat, vertex.lon):
             return False
 
         if len(self.points) < self.capacity:
-            self.points.append(point)
+            self.points.append(vertex)
             return True
         else:
             if not self.divided:
                 self.subdivide()
 
-            if self.nw.insert(point) or self.ne.insert(point) or self.sw.insert(point) or self.se.insert(point):
+            if self.northwest.insert(vertex):
+                return True
+            elif self.northeast.insert(vertex):
+                return True
+            elif self.southwest.insert(vertex):
+                return True
+            elif self.southeast.insert(vertex):
                 return True
 
         return False
 
-    def in_boundary(self, point):
-        x, y, w, h = self.boundary
-        return x <= point.x < x + w and y <= point.y < y + h
+    def in_boundary(self, lat, lon):
+        min_lat, min_lon, max_lat, max_lon = self.boundary
+        return min_lon <= lon < max_lon and min_lat <= lat < max_lat
 
     def query_range(self, range, found=None):
         if found is None:
@@ -89,7 +95,7 @@ class QuadTree:
         # find closest vertex to the given latitude and longitutde
         while True:
             range_to_query = (lat - search_radius, lon - search_radius, search_radius *2, search_radius*2)
-            found_points = self.query_range(range(range_to_query))
+            found_points = self.query_range(range_to_query)
             
             if found_points:
                 return min(found_points, key=lambda p: Graph.getDistance(p.lat, p.lon, lat, lon))
